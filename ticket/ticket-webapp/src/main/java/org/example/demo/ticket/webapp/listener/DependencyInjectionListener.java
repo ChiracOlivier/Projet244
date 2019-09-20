1cdd33002675027fe03feafb908ec3e88f5bad7e
Chiracolivier <chiracolivier28@gmail.com>
2019-09-20 09:24:30 +0200

    injection de dependances grace a un listener de servlet.

--- ticket/ticket-webapp/src/main/java/org/example/demo/ticket/webapp/listener/DependencyInjectionListener.java
+++ ticket/ticket-webapp/src/main/java/org/example/demo/ticket/webapp/listener/DependencyInjectionListener.java
@@ -1,4 +1,23 @@
 package org.example.demo.ticket.webapp.listener;
 
-public class DependencyInjectionListener {
+import org.example.demo.ticket.business.manager.ManagerFactory;
+import org.example.demo.ticket.webapp.rest.resource.ticket.AbstractResource;
+
+import javax.servlet.ServletContextEvent;
+import javax.servlet.ServletContextListener;
+
+public class DependencyInjectionListener implements ServletContextListener {
+
+    @Override
+    public void contextInitialized (ServletContextEvent sce){
+        //Creétaion de l'instance de ManagerFactory
+        ManagerFactory vManagerFactory = new ManagerFactory();
+
+        //Injection de l'instance de ManagerFactory dans la classe AbstractResource
+        AbstractResource.setManagerFactory(vManagerFactory);
+    }
+    @Override
+    public void contextDestroyed(ServletContextEvent sce){
+
+    }
 }
